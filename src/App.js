23d85b5458c698789683fc5c7eeedc30ad1cc831
Nandak <nandak@nextgen.com>
2019-12-16 18:57:36 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,66 +1,15 @@
 import React from 'react';
+import ProfileApp from "./components/profile/ProfileApp";
 import './App.css';
-import Counter from "./components/counter/Counter"
+import './bootstrap.css';
+
 
 function App() {
     return (
         <div>
-            <Counter></Counter>
+            <ProfileApp/>
         </div>
     )
 }
-
-/*
-class LearningComponents extends Component {
-    render() {
-        return (
-            <div className="App">
-                My Hello World
-                <FirstComponent></FirstComponent>
-                <SecondComponent></SecondComponent>
-                <ThirdComponent/>
-                <FourthComponent/>
-            </div>
-        )
-    }
-}
-*/
-
-/*class SecondComponent extends Component{
-    render(){
-        return(
-            <div className="secondComponent">
-                SecondComponent
-            </div>
-        );
-    }
-}
-*/
-
-// class ThirdComponent extends Component{
-//     render(){
-//         return(
-//             <div className="thirdComponent">
-//                 ThirdComponent
-//             </div>
-//         );
-//     }
-// }
-/*
-function ThirdComponent(){
- return (
-     <div className="thirdComponent">
-         Third Component
-     </div>
- );
-}*/
-
-/*function FourthComponent() {
-    return (
-        <div className="fourthComponent">
-            Fourth Component
-        </div>
-    )
-}*/
 
 export default App;
