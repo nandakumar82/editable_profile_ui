4ff861bb39325ece1cba6d3760cb16c629156fd7
Nandak <nandak@nextgen.com>
2019-12-18 13:40:47 +0530

    Error Component

--- src/components/profile/ErrorComponent.jsx
+++ src/components/profile/ErrorComponent.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import {Link} from "react-router-dom";
 
 function ErrorComponent() {
-    return <div>An Error Occurred. I don't know what to do! Contact support at <a>nandakumar82@gmail.com</a></div>
+    return <div>An Error Occurred. I don't know what to do! Contact support at <Link>nandakumar82@gmail.com</Link></div>
 }
 
 export default ErrorComponent
