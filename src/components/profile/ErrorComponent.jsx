f95ed8100bdff848acf9b6fbc939dd5dfaea7379
Nandak <nandak@nextgen.com>
2019-12-18 09:34:56 +0530

    Error Component

---
+++ src/components/profile/ErrorComponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function ErrorComponent() {
+    return <div>An Error Occurred. I don't know what to do! Contact support at <a>nandakumar82@gmail.com</a></div>
+}
+
+export default ErrorComponent
