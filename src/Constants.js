a197ca961b613769e19f867952122bb19188a99b
Nandak <nandak@nextgen.com>
2019-12-18 21:40:47 +0530

    Change the URL to point to the remote service deployed on Heroku

--- src/Constants.js
+++ src/Constants.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:8081/api'
-export const HEROKU_API_URL = 'https://editableprofilebackend.herokuapp.com/api'
+// export const API_URL = 'http://localhost:8081/api'
+export const API_URL = 'https://editableprofilebackend.herokuapp.com/api'
